d280e55bedbed2323481ee4a3641893710b67404
Valtteri Vuorio <vuoriov4@gmail.com>
2021-09-11 17:19:12 +0300

    Create 0328-odd-even-linked-list.cpp

---
+++ 0328-odd-even-linked-list.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *   int val;
+ *   ListNode *next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* oddEvenList(ListNode* head) {
+    if (head == nullptr) return nullptr;
+    ListNode* odd = head; 
+    if (head->next == nullptr) return head;
+    ListNode* even = head->next;
+    while (even != nullptr) {
+      ListNode* current = even->next;
+      if (current == nullptr) break;
+      cut(even, current, odd);
+      even = even->next;
+      odd = current;
+    }
+    return head; 
+  }
+  void cut(ListNode* x_prev, ListNode* x, ListNode* y) {
+    // connect x_prev to x.next
+    x_prev->next = x->next;
+    // connect x.next to y.next
+    x->next = y->next;
+    // connext y.next to x
+    y->next = x;
+  }
+};
